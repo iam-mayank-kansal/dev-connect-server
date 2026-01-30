725b8546e10128ce3fbf57520a9dab84db4a4294
iam-mayank-kansal <iam.mayank.kansal.01@gmail.com>
2026-01-30 20:43:48 +0530

    fix: add MongoDB connection options for serverless compatibility

--- config/database.js
+++ config/database.js
@@ -3,7 +3,14 @@ const logger = require("../helper/logger");
 
 async function connectToDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      maxPoolSize: 5,
+      minPoolSize: 1,
+      retryWrites: true,
+      w: "majority",
+    });
     logger.log({
       level: "info",
       message: "DB Connection Successful",
