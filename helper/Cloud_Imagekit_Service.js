ed4db5837fee8cb65ed7378dc529a28cd9304dde
iam-mayank-kansal <iam.mayank.kansal.01@gmail.com>
2026-01-31 16:15:44 +0530

    cleanup: remove unused class-transformer dependency and Cloud_Imagekit_Service.js

--- helper/Cloud_Imagekit_Service.js
+++
@@ -1,7 +0,0 @@
-// var ImageKit = require("imagekit");
-
-// var imagekit = new ImageKit({
-//   publicKey: "public_y53Z8llGnJaXruvVBdV2i0Gvc3I=",
-//   privateKey: "private_PAaNJ/a4ZFM3nc7jtGw0kQhrkn8=",
-//   urlEndpoint: "https://ik.imagekit.io/devconnect",
-// });
